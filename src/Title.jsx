213e0073f01c6d0f32e3f264959c6a663e1ca1c7
Don Joseph <djoseph@opentext.com>
2025-09-02 16:35:21 -0400

    add title

---
+++ src/Title.jsx
@@ -0,0 +1,12 @@
+
+import './Title.css'
+
+function Title(){
+    return(
+        <div className='title-container'>
+            <h1>RPA Dashboard</h1>
+        </div>
+    );
+}
+
+export default Title
